be95a9ac7d51076a1387fa629c01d516aa9b801f
Yuan <byrd.zhang@gmail.com>
2019-02-19 23:51:09 -0500

    Create HW04a - Yuan Zhang.py

---
+++ HW04a - Yuan Zhang.py
@@ -0,0 +1,54 @@
+"""
+Author: Yuan Zhang
+Course: SSW 567
+Description of this script: HW04a - Develop with the Perspective of the Tester in mind
+"""
+
+import urllib.request
+import json
+import unittest 
+
+def GitHubRetriver(user_id):
+    repo_url = 'https://api.github.com/users/{}/repos'.format(user_id)
+    try:
+        urlhand = urllib.request.urlopen(repo_url)
+    except ValueError:
+        print('The GitHub API cannot be opened. Please check your input or the availability of the website')
+    else:
+            data = urlhand.read().decode()
+            info = json.loads(data)
+            repo_pair = {}
+            for repo in info:
+                repo_name = repo["name"]
+                commit_url = 'https://api.github.com/repos/{}/{}/commits'.format(user_id, repo_name)
+                urlhand = urllib.request.urlopen(commit_url)
+                data = urlhand.read().decode()
+                info = json.loads(data)
+                commits_num = len(info)
+                repo_pair[repo_name] = commits_num
+            return repo_pair
+            
+
+def run_GitHubRetriver(user_id):
+    dic = GitHubRetriver(user_id)
+    for key in dic:
+        print('[Repo] {} [Number of commits] {}'.format(key, dic[key]))
+    
+
+class testGitHubRetriver(unittest.TestCase):
+    def testKybeth(self):
+        self.assertEqual(len(GitHubRetriver('Kybeth')), 9)
+        self.assertEqual(sorted(GitHubRetriver('Kybeth').keys())[0], 'auto-sms-by-twilio')
+        self.assertEqual(GitHubRetriver('Kybeth')['auto-sms-by-twilio'], 3)
+        
+    def testrichkempinski(self):
+        self.assertDictEqual(GitHubRetriver('richkempinski'), {})##
+
+
+if __name__ == '__main__':
+    # examples of running the code
+    run_GitHubRetriver("richkempinski")
+    
+    #unittest.main(exit=False) # this runs all of the tests - use this line if running from Spyder
+    unittest.main(exit=True) # this runs all of the tests - use this line if running from the command line
+    
